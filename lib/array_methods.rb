01eb2460bede162055ad7d13cd53d7c90fe50573
gregarious-number-3828 <muse.malize@gmail.com>
2019-08-26 19:25:05 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,47 @@
+# Method. Find element index.
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  array.length.times { |index|
+    if array[index] == value_to_find
+      return index
+    end
+  }
+  
+  # Returns nil if value_to_find cannot be found
+  return nil
+  
 end
 
+
+# Method. Find max value.
 def find_max_value(array)
-  # Add your solution here
+
+  max_value = 0 
+  
+  array.length.times { |index|
+    if array[index] > max_value
+      max_value = array[index]
+    end
+  }
+  
+  return max_value
+  
 end
 
+
+# Method. Find min value.
 def find_min_value(array)
-  # Add your solution here
+  
+  min_value = array[0]
+  
+  array.length.times { |index|
+    if array[index] < min_value
+      min_value = array[index]
+    end
+  }
+  
+  return min_value
+  
 end
+
+
