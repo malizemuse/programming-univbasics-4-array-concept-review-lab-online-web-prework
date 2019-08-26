55c3c5a7e3ff754542c7710ce9c5a4a71e88f3d6
gregarious-number-3828 <muse.malize@gmail.com>
2019-08-26 19:07:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
+
   array.length.times { |index|
     if array[index] == value_to_find
       return index
+
   }
 end
 
@@ -12,3 +14,7 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
+
+# Scrap Code 
+
